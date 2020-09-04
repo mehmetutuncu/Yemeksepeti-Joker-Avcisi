be6d67678809c11f2558831244440c1fc40ecf40
mehmet.tutuncu <mehmet.tutuncu@elsitel.com.tr>
2020-09-04 14:54:05 +0300

    Bulunan şokerlerin puan durumlarını ve istenen ödeme seçeneğini içerip içermediklerini kontrol edip loglayan yapı eklendi.

--- README.md
+++ README.md
@@ -9,4 +9,38 @@ Program kendisi yemeksepeti.db dosyasını oluşturacaktır.<br>
 
 ### 3- Programı Çalıştırma
 Konsola ```python main.py``` yazarak çalıştırıyoruz. 
-Programın joker bulması durumunda konsol üzerinde yönergeler geliyor. 
+Programın joker bulması durumunda konsol üzerinde yönergeler geliyor.<br> 
+restaurant_info.json dosyasının içerisinde paymentAccepted anahtarında ödeme seçenekleri gelmekte. 
+İsteğinize göre kod içerisinden revize edebilirsiniz.
+
+### 4- Output
+````commandline
+INFO:root:Yıldız Hilal Mah. joker aranıyor...
+INFO:root:Yıldız Hilal Mah. joker bulundu!
+
+INFO:root:
+Restorant Adı: Madame Waffle
+Ödeme Şekli: Ticket Restaurant Yemek Kartı geçerli değildir.
+Adres:Çankaya (Birlik Mah.), Ankara
+Hız: 9,4 / Servis: 9,4 / Lezzet: 9,3
+
+INFO:root:
+Restorant Adı: Tencere Ev Yemekleri
+Ödeme Şekli: Ticket Restaurant Yemek Kartı geçerlidir.
+Adres:G.O.P (Nenehatun), Ankara
+Hız: 8,7 / Servis: 8,9 / Lezzet: 8,8
+
+INFO:root:
+Restorant Adı: Dicle Pide & Kebap
+Ödeme Şekli: Ticket Restaurant Yemek Kartı geçerli değildir.
+Adres:Çankaya (Güzeltepe Mah.), Ankara
+Hız: 8,5 / Servis: 8,6 / Lezzet: 8,3
+
+INFO:root:
+Restorant Adı: Light Fit Sports Cafe
+Ödeme Şekli: Ticket Restaurant Yemek Kartı geçerlidir.
+Adres:Yukarı Ayrancı, Ankara
+Hız: 9,2 / Servis: 9,3 / Lezzet: 9,2
+
+Geçmek istiyor musunuz? e/h: 
+````
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from models import db, User, Region
 from time import sleep
 import logging
 from random import choice
+from bs4 import BeautifulSoup
+import requests
+import json
 
 logging.basicConfig(level=logging.INFO)
 
@@ -12,6 +15,7 @@ class JokerAvcisi:
     def __init__(self):
         self.browser = None
         self.base_url = "https://www.yemeksepeti.com/ankara"
+        self.url = "https://www.yemeksepeti.com"
         self.is_authenticated = False
         self.open_connection()
         self.init_database()
@@ -63,6 +67,31 @@ class JokerAvcisi:
         if self.is_authenticated is True:
             self.loop_regions()
 
+    def check_criteria(self):
+        soup = BeautifulSoup(self.browser.page_source, 'lxml')
+        willcards = soup.select('a.joker-offer-item')
+        data = []
+        for willcard in willcards:
+            restaurant_url = f"{self.url}{willcard.get('data-href')}"
+            response = requests.get(restaurant_url)
+            if response.status_code == 200:
+                _soup = BeautifulSoup(response.content, 'lxml')
+                points = _soup.select('span.point')
+                point_text = f"Hız: {points[0].text} / Servis: {points[1].text} / Lezzet: {points[2].text}"
+                scripts = _soup.select('script[type="application/ld+json"]')
+                _json = None
+                for script in scripts:
+                    _json = json.loads(script.string)
+
+                    if 'paymentAccepted' in _json:
+                        if 'Ticket Restaurant Yemek Kartı' in _json['paymentAccepted']:
+                            _json['status'] = True
+                        else:
+                            _json['status'] = False
+                        _json['points'] = point_text
+                        data.append(_json)
+        return data
+
     def loop_regions(self):
         regions = Region.select()
         if len(regions) > 0:
@@ -71,11 +100,26 @@ class JokerAvcisi:
                     region = choice(regions)
                     logging.info(f"{region.region_name} joker aranıyor...")
                     self.browser.get(region.region_url)
-                    sleep(5)
+                    sleep(3)
                     wildcard_logo = self.browser.find_element_by_class_name("jokerLogo")
                     if wildcard_logo is not None:
                         logging.info(f"{region.region_name} joker bulundu!\n")
                         sleep(0.2)
+                        criteria_data = self.check_criteria()
+
+                        for data in criteria_data:
+                            if data['status'] is True:
+                                text = f"\nRestorant Adı: {data['name']}\n" \
+                                       f"Ödeme Şekli: Ticket Restaurant Yemek Kartı geçerlidir.\n" \
+                                       f"Adres:{data['address']['addressLocality']}\n" \
+                                       f"{data['points']}\n"
+                            else:
+                                text = f"\nRestorant Adı: {data['name']}\n" \
+                                       f"Ödeme Şekli: Ticket Restaurant Yemek Kartı geçerli değildir.\n" \
+                                       f"Adres:{data['address']['addressLocality']}\n" \
+                                       f"{data['points']}\n"
+                            logging.info(text)
+                        sleep(2)
                         status = input("Geçmek istiyor musunuz? e/h: ")
                         if status == 'e':
                             self.browser.find_element_by_id("cboxClose").click()
@@ -95,3 +139,4 @@ class JokerAvcisi:
 if __name__ == '__main__':
     ja = JokerAvcisi()
     ja.main()
+    # ja.check_criteria()
